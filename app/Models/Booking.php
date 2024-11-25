67cab31468d08c91efbcf921e908727b34d88d3b
datamucho <datamucho3@gmail.com>
2024-11-25 23:24:48 +0100

    Booking schema-model

---
+++ app/Models/Booking.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'check_in',
+        'check_out',
+        'guests',
+        'special_requests',
+        'total_price',
+        'status'
+    ];
+
+    protected $casts = [
+        'check_in' => 'date',
+        'check_out' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+} 
